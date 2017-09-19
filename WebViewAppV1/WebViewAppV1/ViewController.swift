acd8758f6f7e8c6e656f455af10484c6f0ac0015
RobJWong <RobertJWong246@gmail.com>
2017-09-19 02:41:40 -0400

    added back, forward and reload functionality

--- WebViewAppV1/WebViewAppV1/ViewController.swift
+++ WebViewAppV1/WebViewAppV1/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
     var webView: WKWebView?
     
     @IBOutlet weak var urlField: UITextField!
     @IBOutlet weak var barView: UIView!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        webView?.goBack()
+    }
+    
+    @IBAction func forward(_ sender: UIBarButtonItem) {
+        webView?.goForward()
+    }
+    
+    @IBAction func reload(_ sender: UIBarButtonItem) {
+        let request = URLRequest(url: (webView?.url)!)
+        webView!.load(request)
+    }
     
     override func loadView() {
         webView = WKWebView()
@@ -24,9 +40,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         barView.frame = CGRect(x:0, y: 0, width: view.frame.width, height: 30)
         // Do any additional setup after loading the view, typically from a nib.
+        webView?.navigationDelegate = self as WKNavigationDelegate
         let url = URL(string:"https://en.wikipedia.org/wiki/Main_Page")
         let request = URLRequest(url: url!)
         webView!.load(request)
+        
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -47,83 +65,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
         return false
     }
-    
-    func displayWebPage(url: String) {
-        
-    }
 
 }
-
-//    var webView: WKWebView?
-//    
-//    @IBOutlet weak var barView: UIView!
-//    @IBOutlet weak var urlField: UITextField!
-//    @IBOutlet weak var backButton: UIBarButtonItem!
-//    @IBOutlet weak var forwardButton: UIBarButtonItem!
-//    @IBOutlet weak var reloadButton: UIBarButtonItem!
-//    
-//    @IBAction func back(_ sender: UIBarButtonItem) {
-//        webView?.goBack()
-//    }
-//    
-//    @IBAction func foward(_ sender: UIBarButtonItem) {
-//        webView?.goForward()
-//    }
-//    
-//    @IBAction func reload(_ sender: UIBarButtonItem) {
-//        let request = URLRequest(url:(webView?.url!)!)
-//        webView?.load(request)
-//    }
-//    
-//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//        if (keyPath == "loading") {
-//            backButton.isEnabled = (webView?.canGoBack)!
-//            forwardButton.isEnabled = (webView?.canGoForward)!
-//        }
-//    }
-//    
-//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-//        barView.frame = CGRect(x: 0, y: 0, width: size.height, height: 30)
-//    }
-//    
-//    override func loadView() {
-//        let webConfiguration = WKWebViewConfiguration()
-//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-//        webView?.uiDelegate = self
-//        //view can be initalized in loadView or viewDidLoad. Don't know the difference between the two
-//        view = webView
-//        
-//        print ("WebViewController loaded its view.")
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        barView.frame = CGRect(x:0, y:0, width: view.frame.width, height: 30)
-//        //webView?.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
-//        webView?.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
-//        // Do any additional setup after loading the view, typically from a nib.
-//        let url = URL(string:"https://9gag.com/trending")
-//        let request = URLRequest(url: url!)
-//        webView!.load(request)
-//        
-//        backButton.isEnabled = false
-//        forwardButton.isEnabled = false
-//    }
-//    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        urlField.resignFirstResponder()
-//        if let unformattedURLField = urlField.text {
-//            if !unformattedURLField.hasPrefix("http://") || !unformattedURLField.hasPrefix("https://") {
-//                let formattedUrlField = "https://" + unformattedURLField
-//                let searchURL = URL(string: formattedUrlField)
-//                webView!.load(URLRequest(url: searchURL!))
-//            }
-//        }
-//        return false
-//    }
-
